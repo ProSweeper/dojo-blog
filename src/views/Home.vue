b207b5f22c2d006b8161f4fb3a7931b73c74232b
ProSweeper <dylan.doucet28@gmail.com>
2023-04-09 17:19:45 -0600

    impliment first composable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,25 @@
 <template>
   <h1>Home</h1>
-  <PostList :posts="posts" v-if="showPosts"/>
-  <button  @click="showPosts = !showPosts">Toggle Posts</button>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="posts.length">
+    <PostList :posts="posts"/>
+  </div>
+  <div v-else>Loading</div>
 </template>
 
 <script>
   import PostList from '@/components/PostList.vue'
-  import { ref } from 'vue'
+  import getPosts from '../composables/getPosts'
 
   export default {
     name: "Home",
     components: {PostList},
     setup() {
-      const posts = ref([
-        { title: "welcome to the blog", body: "lorem ipsum", id: 1},
-        { title: "Top 5 CSS Tips", body: "lorem ipsum", id: 2},
-      ])
-      const showPosts = ref(true)
-      return { posts, showPosts }
+      const { posts, error, load } = getPosts()
+
+      load()
+
+      return { posts, error }
     }
   }
 </script>
