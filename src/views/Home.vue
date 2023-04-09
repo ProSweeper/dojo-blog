94182064f9a564ac902db18b565509587611eab7
ProSweeper <dylan.doucet28@gmail.com>
2023-04-09 16:28:58 +0000

    practice with lifecycle hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>home</h1>
+  <h1>Home</h1>
+  <PostList :posts="posts" v-if="showPosts"/>
+  <button  @click="showPosts = !showPosts">Toggle Posts</button>
 </template>
+
+<script>
+  import PostList from '@/components/PostList.vue'
+  import { ref } from 'vue'
+
+  export default {
+    name: "Home",
+    components: {PostList},
+    setup() {
+      const posts = ref([
+        { title: "welcome to the blog", body: "lorem ipsum", id: 1},
+        { title: "Top 5 CSS Tips", body: "lorem ipsum", id: 2},
+      ])
+      const showPosts = ref(true)
+      return { posts, showPosts }
+    }
+  }
+</script>
